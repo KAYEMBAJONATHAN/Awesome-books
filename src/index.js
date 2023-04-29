ab847d0a6e793c645cf6d344f2b34d9227fd7241
kayemba <kayembejonathan5@gmail.com>
2023-04-29 17:31:53 +0200

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './index.css';
-import { DateTime } from 'luxon';
 import navigation from './modules/navigation';
 import books from './modules/books';
 import displayBook from './modules/displayBook';
 
-displayBook();
-navigation();
-books();
+const { DateTime } = require('luxon');
 
 const timeContent = document.getElementById('time');
 timeContent.textContent = DateTime.now().toISO();
+
+displayBook();
+navigation();
+books();
--- src/modules/displayBook.js
+++ src/modules/displayBook.js
@@ -1,16 +1,17 @@
-const booklist = document.querySelector('.booklist');
-let bookArray = [];
-if (localStorage.getItem('bookOfLife')) {
-  bookArray = JSON.parse(localStorage.getItem('bookOfLife')); console.log(bookArray);
-}
+const displayBook = () => {
+  const booklist = document.querySelector('.booklist');
+  let bookArray = [];
+  if (localStorage.getItem('bookOfLife')) {
+    bookArray = JSON.parse(localStorage.getItem('bookOfLife')); 
+  }
 
-let content = '';
-if (bookArray.length > 0) {
-  bookArray.forEach((element) => {
-    content += `<li>${element.title} ${element.author} <span>remove</span></li>`;
-  });
-  booklist.innerHTML = content;
-  console.log(bookArray);
-}
+  let content = '';
+  if (bookArray.length > 0) {
+    bookArray.forEach((element) => {
+      content += `<li>${element.title} ${element.author} <span>remove</span></li>`;
+    });
+    booklist.innerHTML = content;
+  }
+};
 
 export default displayBook;
