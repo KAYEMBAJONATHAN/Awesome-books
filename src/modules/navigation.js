64f199c5aad2c395ee175e59f646c524af49c4e0
kayemba <kayembejonathan5@gmail.com>
2023-05-01 18:13:02 +0200

    fixed eslint

--- src/modules/navigation.js
+++ src/modules/navigation.js
@@ -1,27 +1,27 @@
 const navigation = () => {
-const listLink = document.getElementById('list');
-const addLink = document.getElementById('add');
-const contactLink = document.getElementById('contact');
-const formSection = document.querySelector('.form-control');
-const contact = document.querySelector('.contact');
-const booklist = document.querySelector('.booklist');
+  const listLink = document.getElementById('list');
+  const addLink = document.getElementById('add');
+  const contactLink = document.getElementById('contact');
+  const formSection = document.querySelector('.form-control');
+  const contact = document.querySelector('.contact');
+  const booklist = document.querySelector('.booklist');
 
-listLink.addEventListener('click', () => {
-  booklist.style.display = 'block';
-  formSection.style.display = 'none';
-  contact.style.display = 'none';
-});
+  listLink.addEventListener('click', () => {
+    booklist.style.display = 'block';
+    formSection.style.display = 'none';
+    contact.style.display = 'none';
+  });
 
-addLink.addEventListener('click', () => {
-  booklist.style.display = 'none';
-  formSection.style.display = 'block';
-  contact.style.display = 'none';
-});
+  addLink.addEventListener('click', () => {
+    booklist.style.display = 'none';
+    formSection.style.display = 'block';
+    contact.style.display = 'none';
+  });
 
-contactLink.addEventListener('click', () => {
-  booklist.style.display = 'none';
-  formSection.style.display = 'none';
-  contact.style.display = 'block';
-});
-}
+  contactLink.addEventListener('click', () => {
+    booklist.style.display = 'none';
+    formSection.style.display = 'none';
+    contact.style.display = 'block';
+  });
+};
 export default navigation;
